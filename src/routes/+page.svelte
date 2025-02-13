aa33a31a47aa3a6f764ed85a04b7b09b346c185c
PippoTeru <teru.pippo.teru@gmail.com>
2025-02-13 16:58:56 +0900

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-	export let data;
-
-	console.log(data);
 </script>
 
 <h1>a</h1>
--- src/routes/+page.ts
+++
@@ -1,7 +0,0 @@
-export async function load() {
-	const response = await fetch('https://bestdori.com/api/songs/all.7.json');
-
-	return {
-		posts: response.ok && (await response.json())
-	};
-}
