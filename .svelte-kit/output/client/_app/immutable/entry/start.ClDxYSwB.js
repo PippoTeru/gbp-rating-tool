77d94315238906d51fcd912b8c05118ae6ebb011
PippoTeru <teru.pippo.teru@gmail.com>
2025-02-13 16:13:21 +0900

    test

---
+++ .svelte-kit/output/client/_app/immutable/entry/start.ClDxYSwB.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/D10FJ1n4.js";export{t as start};
---
+++ .svelte-kit/output/server/manifest.js
@@ -0,0 +1,28 @@
+export const manifest = (() => {
+function __memo(fn) {
+	let value;
+	return () => value ??= (value = fn());
+}
+
+return {
+	appDir: "_app",
+	appPath: "gbp-rating-tool/_app",
+	assets: new Set(["favicon.png"]),
+	mimeTypes: {".png":"image/png"},
+	_: {
+		client: {start:"_app/immutable/entry/start.ClDxYSwB.js",app:"_app/immutable/entry/app.DcIbEipM.js",imports:["_app/immutable/entry/start.ClDxYSwB.js","_app/immutable/chunks/D10FJ1n4.js","_app/immutable/chunks/r3sb4fzG.js","_app/immutable/chunks/Bo0XT3TE.js","_app/immutable/entry/app.DcIbEipM.js","_app/immutable/chunks/r3sb4fzG.js","_app/immutable/chunks/CIgllM7I.js","_app/immutable/chunks/Doc5R4-m.js","_app/immutable/chunks/Bo0XT3TE.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		nodes: [
+			
+		],
+		routes: [
+			
+		],
+		prerendered_routes: new Set(["/gbp-rating-tool/"]),
+		matchers: async () => {
+			
+			return {  };
+		},
+		server_assets: {}
+	}
+}
+})();
